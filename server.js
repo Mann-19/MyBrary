cf26885933fb72b1f180d63e9d97a9434a76ad06
Mann-19 <tkp2510@gmail.com>
2023-06-23 00:08:04 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + "/views");
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 const mongoose = require("mongoose");
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL);
-  
+    await mongoose.connect(process.env.DATABASE_URL);
 }
+const db = mongoose.connection;
+db.once('open', () => console.log("Connected to MongoDB.."))
 
 app.listen(process.env.PORT || 3000);
